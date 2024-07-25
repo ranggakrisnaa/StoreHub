a2545e04d53b73b7b16cf7db917af8fb78812ae6
ranggakrisnaa <infinity.duos@gmail.com>
2024-07-25 10:30:44 +0700

    fix: refactory user file model

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -3,44 +3,27 @@ import {
     Post,
     Body,
     InternalServerErrorException,
-    UnauthorizedException,
     Res,
     HttpStatus,
     NotFoundException,
     BadRequestException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/register-user.dto';
-import { BcryptService } from 'src/bcrypt/bcrypt.service';
 import { Response } from 'express';
 import { LoginUserDto } from './dto/login-user.dto';
 import { Prisma } from '@prisma/client';
 import { VerifyOtpDto } from './dto/verify-otp.dto';
-import { JwtPayload } from 'src/auth/interface/jwt.inteface';
 import { OtpService } from 'src/otps/otp.service';
 import { UserService } from './user.service';
 
 @Controller('v1/users')
 export class UserController {
-    constructor(
-        private readonly usersService: UserService,
-        private readonly bcryptService: BcryptService,
-        private readonly otpService: OtpService,
-    ) {}
+    constructor(private readonly usersService: UserService) {}
 
     @Post('register')
     async register(@Body() createUserDto: CreateUserDto, @Res() res: Response): Promise<Record<string, any>> {
-        const { email, name, username, role, password, confirmPassword } = createUserDto;
         try {
-            if (password !== confirmPassword) throw new UnauthorizedException('Password is not match');
-            const hashPassword: string = await this.bcryptService.hashPassword(confirmPassword);
-
-            const data: Prisma.UserCreateInput = await this.usersService.createUser({
-                email,
-                username,
-                name,
-                password: hashPassword,
-                role,
-            });
+            const data = await this.usersService.register(createUserDto);
 
             return res.status(HttpStatus.CREATED).json({
                 success: true,
@@ -49,39 +32,14 @@ export class UserController {
                 data,
             });
         } catch (error) {
-            if (error !== InternalServerErrorException) {
-                throw error;
-            } else {
-                throw new InternalServerErrorException(error.message);
-            }
+            throw new InternalServerErrorException(error.message);
         }
     }
 
     @Post('login')
     async login(@Body() loginUserDto: LoginUserDto, @Res() res: Response): Promise<Record<string, any>> {
-        const { usernameOrEmail, password } = loginUserDto;
-        const data: Prisma.UserGetPayload<{}> = await this.usersService.findUser({
-            OR: [{ email: usernameOrEmail }, { username: usernameOrEmail }],
-        });
-        if (!data) throw new NotFoundException('User name or email is not found.');
-
         try {
-            const checkPassword: boolean = await this.bcryptService.comparePassword(password, data.password);
-            if (!checkPassword) throw new BadRequestException('Password is invalid.');
-
-            if (await this.otpService.checkDayLimit(data.email, 'login'))
-                throw new BadRequestException('Batas percobaan harian telah tercapai.');
-            if (await this.otpService.checkThrottle(data.email, 'login'))
-                throw new BadRequestException(
-                    'Anda telah melebihi batas dalam permintaan otp. Coba lagi dalam 1 menit.',
-                );
-
-            const otp: string = await this.otpService.sendEmailOtp(data.email);
-            const payload: Prisma.OtpCreateWithoutUserInput = {
-                otp: parseInt(otp),
-                type: 'login',
-            };
-            await this.otpService.saveOtp(payload, data.id);
+            const { data, otp } = await this.usersService.login(loginUserDto);
 
             return res.status(HttpStatus.OK).json({
                 success: true,
@@ -91,34 +49,14 @@ export class UserController {
                 email: data.email,
             });
         } catch (error) {
-            if (error !== InternalServerErrorException) {
-                throw error;
-            } else {
-                throw new InternalServerErrorException(error.message);
-            }
+            throw new InternalServerErrorException(error.message);
         }
     }
 
     @Post('verify-otp')
     async verify(@Body() verifyOtpDto: VerifyOtpDto, @Res() res: Response): Promise<Record<string, any>> {
-        const { email, otp } = verifyOtpDto;
-        const data: Prisma.UserGetPayload<{}> = await this.usersService.findUser({
-            email,
-        });
-        if (!data) throw new NotFoundException('User email is not found.');
-
         try {
-            const verify: boolean = await this.otpService.verifyOtp(data.id, 'login', otp);
-            if (!verify) throw new BadRequestException('Otp expired or invalid.');
-
-            const accessToken: string = await this.usersService.generateAccessToken(data);
-            const refreshToken: string = await this.usersService.generateRefreshToken(data);
-
-            const payload: Prisma.TokenCreateWithoutUserInput = {
-                accessToken,
-                refreshToken,
-            };
-            await this.usersService.saveToken(payload, data.id);
+            const { accessToken, refreshToken } = await this.usersService.verifyOtp(verifyOtpDto);
 
             return res.status(HttpStatus.OK).json({
                 success: true,
@@ -130,11 +68,15 @@ export class UserController {
                 },
             });
         } catch (error) {
-            if (error !== InternalServerErrorException) {
-                throw error;
-            } else {
-                throw new InternalServerErrorException(error.message);
-            }
+            throw new InternalServerErrorException(error.message);
+        }
+    }
+
+    @Post('refresh')
+    async refresh() {
+        try {
+        } catch (error) {
+            throw new InternalServerErrorException(error.message);
         }
     }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,15 +1,82 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, UnauthorizedException, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-import { Prisma, Token, User } from '@prisma/client';
+import { Otp, Prisma, Token, User } from '@prisma/client';
 import { JwtPayload } from 'src/auth/interface/jwt.inteface';
 import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { CreateUserDto } from './dto/register-user.dto';
+import { BcryptService } from 'src/bcrypt/bcrypt.service';
+import { LoginUserDto } from './dto/login-user.dto';
+import { OtpService } from 'src/otps/otp.service';
+import { VerifyOtpDto } from './dto/verify-otp.dto';
 
 @Injectable()
 export class UserService {
     constructor(
         private prisma: PrismaService,
         private readonly jwtService: JwtStrategy,
+        private readonly bcryptService: BcryptService,
+        private readonly otpService: OtpService,
     ) {}
+
+    async register(dataInput: CreateUserDto): Promise<User> {
+        const { email, name, username, role, password, confirmPassword } = dataInput;
+        if (password !== confirmPassword) throw new UnauthorizedException('Password is not match');
+        const hashPassword: string = await this.bcryptService.hashPassword(confirmPassword);
+
+        return await this.createUser({
+            email,
+            username,
+            name,
+            password: hashPassword,
+            role,
+        });
+    }
+
+    async login(dataInput: LoginUserDto): Promise<Record<string, any>> {
+        const { usernameOrEmail, password } = dataInput;
+        const data: Prisma.UserGetPayload<{}> = await this.findUser({
+            OR: [{ email: usernameOrEmail }, { username: usernameOrEmail }],
+        });
+        if (!data) throw new NotFoundException('User name or email is not found.');
+        const checkPassword: boolean = await this.bcryptService.comparePassword(password, data.password);
+        if (!checkPassword) throw new BadRequestException('Password is invalid.');
+
+        if (await this.otpService.checkDayLimit(data.email, 'login'))
+            throw new BadRequestException('Batas percobaan harian telah tercapai.');
+        if (await this.otpService.checkThrottle(data.email, 'login'))
+            throw new BadRequestException('Anda telah melebihi batas dalam permintaan otp. Coba lagi dalam 1 menit.');
+
+        const otp: string = await this.otpService.sendEmailOtp(data.email);
+        const payload: Prisma.OtpCreateWithoutUserInput = {
+            otp: parseInt(otp),
+            type: 'login',
+        };
+        await this.otpService.saveOtp(payload, data.id);
+
+        return { data, otp };
+    }
+
+    async verifyOtp(dataInput: VerifyOtpDto): Promise<Record<string, string>> {
+        const { email, otp } = dataInput;
+        const data: Prisma.UserGetPayload<{}> = await this.findUser({
+            email,
+        });
+        if (!data) throw new NotFoundException('User email is not found.');
+
+        const verify: boolean = await this.otpService.verifyOtp(data.id, 'login', otp);
+        if (!verify) throw new BadRequestException('Otp expired or invalid.');
+
+        const accessToken: string = await this.generateAccessToken(data);
+        const refreshToken: string = await this.generateRefreshToken(data);
+
+        const payload: Prisma.TokenCreateWithoutUserInput = {
+            accessToken,
+            refreshToken,
+        };
+        await this.saveToken(payload, data.id);
+
+        return { accessToken, refreshToken };
+    }
 
     async findUser(where: Prisma.UserWhereInput): Promise<User | null> {
         return this.prisma.user.findFirst({
