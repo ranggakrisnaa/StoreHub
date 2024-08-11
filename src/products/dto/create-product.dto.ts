c82bf3d4350757fbce1fdb6f960386781fbebaf5
ranggakrisnaa <infinity.duos@gmail.com>
2024-08-11 21:59:55 +0700

    feat(product): add new create product function

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,27 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+    @IsNumber()
+    @IsNotEmpty()
+    productCategoryId: number;
+
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    quantity: number;
+
+    @IsString()
+    @IsNotEmpty()
+    description: string;
+
+    @IsString()
+    @IsNotEmpty()
+    SKU: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    price: number;
+}
--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, InternalServerErrorException, Post, Request, Res, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { PrismaService } from 'src/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
-@Controller('product')
+@Controller('v1/products')
 export class ProductController {
     constructor(private readonly productService: ProductService) {}
 
     @UseGuards(JwtAuthGuard)
     @Post()
-    async createProduct() {}
+    async createProduct(
+        @Body() createProdutDto: CreateProductDto,
+        @Request() req: Record<any, any>,
+        @Res() res: Response,
+    ) {
+        try {
+        } catch (error) {
+            throw new InternalServerErrorException(error.message);
+        }
+    }
 }
